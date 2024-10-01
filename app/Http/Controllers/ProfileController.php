8ca6282bc63579b2caa4d9a0833623958e0c0675
Mr <aron.fekete25@gmail.com>
2024-10-01 11:40:11 +0200

    ProfileController updated

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Bootstrap\AuthenticatedUser;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Config;
@@ -30,31 +31,33 @@ class ProfileController extends Controller
 	  }
 		
 	  $attributes = [];
-		$user = Auth::user();
+		$mysql_user = (new User)->setConnection("mysql")->find(Auth::user()->id);
+		$sqlite_user = (new User)->setConnection("sqlite")->find(Auth::user()->id);
 		
 		if($request->get('nickname')) {
 			$nickname = $request->validate([ "nickname" => ["required", "string", "max:255"] ])["nickname"];
-			if($user->nickname != $nickname) {
+			if($mysql_user->nickname != $nickname) {
 				$attributes["nickname"] = $nickname;
 			}
 		}
 	  if($request->get('date_of_birth')) {
 			$date_of_birth = $request->validate([ "date_of_birth" => ["required", "date", "date_format:Y-m-d"] ])["date_of_birth"];
-		  if($user->date_of_birth != $date_of_birth) {
+		  if($mysql_user->date_of_birth != $date_of_birth) {
 			  $attributes["date_of_birth"] = $date_of_birth;
 		  }
 	  }
 	  if($request->get('email')) {
 		  $email = $request->validate([ "email" => ["required", "email:rfc,dns", "exists:users"] ])["email"];
-		  if($user->email != $email) {
+		  if($mysql_user->email != $email) {
 			  $attributes["email"] = $email;
 		  }
 	  }
 	  if($request->get('password')) {
 		  $attributes["password"] = $request->validate([ "password" => ["required", "min:6"] ])["password"];
 	  }
-		
-		$user->update($attributes);
+	  
+	  $mysql_user->update($attributes);
+	  $sqlite_user->update($attributes);
 		
 	  $request->session()->regenerate();
 		
